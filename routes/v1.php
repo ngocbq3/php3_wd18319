8a9f26b84048f824ccb46d43ad24f4435af043c6
ngocbq3 <ngocbq@fpt.edu.vn>
2025-05-15 16:31:04 +0700

    Revert "merge v1.php"
    This reverts commit 96e519db7f6e4726eb164368b5e3dedd0b55edfa.

--- routes/v1.php
+++ routes/v1.php
@@ -1,12 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('test', function () {
     return 'V1 OK';
 });
 //Edit route api
-
-
-Route::get('posts', [PostController::class, 'index']);
